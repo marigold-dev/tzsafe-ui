9801c5e27601bbbe06c61fba40a450f02853a885
JulesGuesnon <jules.guesnon@hetic.net>
2023-02-16 18:35:15 +0900

    fix(Layout): Fixed nav at the top and footer at the bottom

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = (_: React.PropsWithChildren) => {
   return (
-    <footer className=" bottom-0 max-h-96 w-full grow-0 border-t-4 border-gray bg-dark text-center lg:text-left">
+    <footer className="absolute bottom-0 left-0 right-0 h-28 border-t-4 border-zinc-600 bg-dark text-center lg:text-left">
       <div className="flex flex-col p-4 text-center text-white">
         <span className=" text-white">
           © {new Date().getFullYear()} Copyright{" "}
