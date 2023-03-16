d9c58fa8ffb2f0033b92ecfe3da6969f3eb07f83
JulesGuesnon <jules.guesnon@hetic.net>
2023-03-16 20:21:57 +0900

    fix(ProposalCard): default value for payload

--- components/ProposalCard.tsx
+++ components/ProposalCard.tsx
@@ -110,10 +110,11 @@ export const RenderProposalContent = ({
         }),
       };
     } else {
+      console.log(metadata);
       const [meta, amount, address, entrypoint, arg] = (() => {
         if (metadata.contract_address) {
           const data = (() => {
-            const entries = Object.entries(metadata.payload);
+            const entries = Object.entries(metadata.payload ?? {});
 
             if (entries.length === 0) return ["default", "{}"];
 
