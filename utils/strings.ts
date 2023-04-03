5b3d604b39767a5e1d39861704e3ec7a9be58bf4
JulesGuesnon <jules.guesnon@hetic.net>
2023-04-03 17:47:12 +0200

    clean: Removed a log

--- utils/strings.ts
+++ utils/strings.ts
@@ -1,8 +1,6 @@
 export const crop = (v: string, maxSize: number): string => {
   const half = Math.ceil((maxSize - 3) / 2);
 
-  console.log(half);
-
   return v.length >= maxSize
     ? `${v.substring(0, half)}...${v.substring(v.length - half)}`
     : v;
