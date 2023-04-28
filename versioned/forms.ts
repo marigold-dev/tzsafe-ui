c732fb2c43e19b6417dd617b7a518285d3bed96a
JulesGuesnon <jules.guesnon@hetic.net>
2023-04-28 12:52:07 +0200

    feat(Settings): Delegation works

--- versioned/forms.ts
+++ versioned/forms.ts
@@ -1,3 +1,5 @@
+import { Expr } from "@taquito/michel-codec";
+
 type ownersForm =
   | {
       addOwners: string[];
@@ -5,6 +7,6 @@ type ownersForm =
   | { removeOwners: string[] }
   | { changeThreshold: number }
   | { adjustEffectivePeriod: number }
-  | { execute_lambda: { metadata?: string; lambda: string } };
+  | { execute_lambda: { metadata?: string; lambda: Expr | null } };
 
 export { type ownersForm };
