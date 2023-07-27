dc532704c04105d7823e847ef6a9658df907861c
Cyril B <cyril.bourdet.pro@gmail.com>
2023-07-27 11:44:11 +0200

    feat (ga): Add google analytics tracking mainnet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,14 @@ import type { AppProps } from "next/app";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/router";
 import { useReducer, useEffect, useState } from "react";
+import ReactGA from "react-ga4";
 import Autocomplete from "../components/Autocomplete";
 import Banner from "../components/Banner";
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/footer";
 import NavBar from "../components/navbar";
 import { PREFERED_NETWORK } from "../context/config";
+import { GA_TRACKING_ID } from "../context/config";
 import {
   tezosState,
   action,
@@ -26,6 +28,8 @@ import {
 } from "../context/state";
 import "../styles/globals.css";
 import Proposals from "./proposals";
+
+ReactGA.initialize(GA_TRACKING_ID);
 
 export default function App({ Component, pageProps }: AppProps) {
   const [state, dispatch]: [tezosState, React.Dispatch<action>] = useReducer(
