3750e210916ea412b8e60b3e0be445c18a67256f
JulesGuesnon <jules.guesnon@hetic.net>
2024-01-16 11:32:36 +0900

    fix(State): Turning numbers into BigNumbers on load

--- types/app.ts
+++ types/app.ts
@@ -1,10 +1,9 @@
 import BigNumber from "bignumber.js";
 import { version } from "./display";
 
-type contractStorage = { version: version } & {
+export type contractStorage = { version: version } & {
   [key: string]: any;
   proposal_counter: BigNumber;
   balance: string;
   threshold: BigNumber;
 };
-export { type contractStorage };
