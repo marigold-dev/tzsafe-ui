7f63877b99f95e2e1a9374e8daee1e1cae7834e2
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2024-03-25 15:28:02 +0100

    sidebar: add dashboard

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -280,6 +280,32 @@ const Sidebar = ({
       </Select.Root>
 
       <div className="mt-8 flex flex-col space-y-4">
+        <Link
+          href={`/${state.currentContract}/dashboard`}
+          className={linkClass(
+            path?.includes("/dashboard") ?? false,
+            isLoading
+          )}
+          onClick={onClose}
+        >
+          <svg
+            width="20px"
+            height="20px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              xmlns="http://www.w3.org/2000/svg"
+              d="M5.77778 10.2222V18C5.77778 19.1046 6.67321 20 7.77778 20H12M5.77778 10.2222L11.2929 4.70711C11.6834 4.31658 12.3166 4.31658 12.7071 4.70711L17.5 9.5M5.77778 10.2222L4 12M18.2222 10.2222V18C18.2222 19.1046 17.3268 20 16.2222 20H12M18.2222 10.2222L20 12M18.2222 10.2222L17.5 9.5M17.5 9.5V6M12 20V15"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <span>Dashboard</span>
+        </Link>
         <a
           href={`https://${PREFERED_NETWORK}.tzkt.io/${state.currentContract}/balances/`}
           target="_blank"
