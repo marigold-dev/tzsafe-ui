c255c43fec2d5744fa3ad82ccd38947ecf021089
Cyril B <cyril.bourdet.pro@gmail.com>
2024-04-10 11:55:18 +0200

    edit config.js to produce docker img

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: false,
   swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
