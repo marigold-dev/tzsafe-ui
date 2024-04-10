2f64327c56e763e30cd358b6a19f06c5bb12a276
Cyril B <cyril.bourdet.pro@gmail.com>
2024-04-10 12:15:09 +0200

    cicd: put back endpoints
    [skip ci]

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: false,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
